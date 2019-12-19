4a1549c02547ac028bade603d46bbfa52d7b821d
guanshixun <501835725@qq.com>
2019-12-19 17:19:11 +0800

    🍕 multi-thread

---
+++ douban/thread.py
@@ -0,0 +1,30 @@
+"""
+多线程请求
+"""
+
+import os
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+from douban import download_cover, get_movies, mkdir_if_not_exsit, pages, per_pages, save_cover
+
+# 封面保存路径
+covers_dir = os.path.join(os.path.dirname(os.path.abspath(__name__)), 'covers', 'thread')
+mkdir_if_not_exsit(covers_dir)
+
+
+def main():
+    with ThreadPoolExecutor() as exector:
+        all_movies = exector.map(get_movies, [i * per_pages for i in range(pages)])
+        for movies in all_movies:
+            for cover in exector.map(download_cover, movies):
+                cover_name = os.path.join(covers_dir, '{}.jpg'.format(cover['title']))
+                cover_data = cover['data']
+                save_cover(cover_name, cover_data)
+                print(cover_name)
+
+
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    print('done', time.time() - start)
